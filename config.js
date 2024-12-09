7f7c54c4cbdcb0b56d00fef09e0bb8038ada0011
Lucas Pereira <chaes_231200@hotmail.com>
2024-12-09 07:55:32 -0300

    change/ajuste de textos e localizacoes do mapa

--- config.js
+++ config.js
@@ -15,23 +15,21 @@ var config = {
     theme: 'dark',
     use3dTerrain: false, //set true for enabling 3D maps.
     auto: false,
-    title: 'Your Title Goes Here',
-    subtitle: 'The Storytelling Template helps you create an awesome animated map story with ease.',
-    byline: 'By a I.M. Amapper',
+    title: 'Protótipo mapa rio 60',
     footer: 'Source: source citations, etc. <br> Created using <a href="https://github.com/mapbox/storytelling" target="_blank">Mapbox Storytelling</a> template.',
     chapters: [
         {
             id: 'slug-style-id',
             alignment: 'left',
             hidden: false,
-            title: 'San Francisco',
-            image: './assets/san-fran.jpeg',
-            description: 'The first chapter contains a title, image, and camera view for San Francisco, California. Update the chapter data to make it your own.',
+            title: 'Ponto 1',
+            image: './assets/placeholder_image.webp',
+            description: 'Descrição ponto 1',
             location: {
-                center: [-122.418398, 37.759483],
-                zoom: 8.5,
+                center: [-43.4096, -22.9035],
+                zoom: 14,
                 pitch: 60,
-                bearing: 0
+                bearing: -17.6
             },
             mapAnimation: 'flyTo',
             rotateAnimation: false,
@@ -54,14 +52,14 @@ var config = {
             id: 'second-identifier',
             alignment: 'right',
             hidden: false,
-            title: 'Washington, D.C.',
-            image: './assets/washington-dc.jpg',
-            description: 'The second chapter flies to Washington, D.C., updates the camera pitch, and slowly rotates. <br>  <br> Washington, D.C., the capital of the United States, is a vibrant city known for its iconic landmarks, including the White House, the U.S. Capitol, and the Washington Monument. It serves as the political heart of the nation and a center for history, culture, and international diplomacy.',
+            title: 'Ponto 2',
+            image: './assets/placeholder_image.webp',
+            description: 'Descrição ponto 2',
             location: {
-                center: [-77.020636, 38.886900],
-                zoom: 8.5,
-                pitch: 60,
-                bearing: -43.2,
+                center: [-43.3096, -22.9032],
+                zoom: 13,
+                pitch: 55,
+                bearing: 180,
                 // flyTo additional controls-
                 // These options control the flight curve, making it move
                 // slowly and zoom out almost completely before starting
@@ -79,14 +77,14 @@ var config = {
             id: 'third-identifier',
             alignment: 'left',
             hidden: false,
-            title: 'Geneva',
-            image: './assets/geneva.jpg',
-            description: 'Geneva, Switzerland, is a picturesque city nestled along the shores of Lake Geneva, surrounded by the Alps and Jura mountains. Known as a global hub for diplomacy and finance, it is home to numerous international organizations, including the United Nations and the Red Cross.',
+            title: 'Ponto 3',
+            image: './assets/placeholder_image.webp',
+            description: 'Descrição ponto 3',
             location: {
-                center: [6.15116, 46.20595],
-                zoom: 12.52,
-                pitch: 8.01,
-                bearing: 0.00
+                center: [ -43.2096, -22.9030],
+                zoom: 12,
+                pitch: 50,
+                bearing: -90
             },
             mapAnimation: 'flyTo',
             rotateAnimation: false,
@@ -98,13 +96,13 @@ var config = {
             id: 'fourth-chapter',
             alignment: 'fully',
             hidden: false,
-            title: 'Buenos Aires',
-            image: './assets/buenos-aires.jpg',
-            description: 'Buenos Aires, the capital of Argentina, is a dynamic city known for its European-inspired architecture, vibrant tango culture, and rich culinary scene. Often called the "Paris of South America," it blends historic charm with modern energy.  You can add as many chapters as you need, just copy the JSON data and make changes.',
+            title: 'Ponto 4',
+            image: './assets/placeholder_image.webp',
+            description: 'Descrição ponto 4',
             location: {
-                center: [-58.54195, -34.71600],
-                zoom: 4,
-                pitch: 0,
+                center: [-43.1096, -22.9028],
+                zoom: 11,
+                pitch: 45,
                 bearing: 0
             },
             mapAnimation: 'flyTo',
